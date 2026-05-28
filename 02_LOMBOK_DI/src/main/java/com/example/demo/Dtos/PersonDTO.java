9bb18b6ca171197a07eab2680fa0f33f808d6919
pyj-0313 <pyjun0313@gmail.com>
2026-05-28 11:13:55 +0900

    02 DAY - SPRING(PersonDTO)

--- 02_LOMBOK_DI/src/main/java/com/example/demo/Dtos/PersonDTO.java
+++ 02_LOMBOK_DI/src/main/java/com/example/demo/Dtos/PersonDTO.java
@@ -3,14 +3,24 @@ package com.example.demo.Dtos;
 // LOMBOK 확인 CTRL + F12
 // JUNIT TEST CASE : CTRL + SHIFT + T
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 
 //@Getter
 //@Setter
 //@ToString
-@Data
+@Data   //Getter, Setter, ToString 3개 한번에 받는 어노테이션
+@AllArgsConstructor // 모든 인자 받는 생성자
+@NoArgsConstructor //기본 생성자
+@Builder
+@Component
 public class PersonDTO {
     private String name;
     private int age;
     private String addr;
+
 }
+
