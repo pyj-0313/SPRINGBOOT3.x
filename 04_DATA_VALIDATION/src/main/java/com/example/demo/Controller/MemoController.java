177a360f6d6cf27ca3e2499fa648744d869166e4
pyj-0313 <pyjun0313@gmail.com>
2026-05-29 10:42:06 +0900

    03 DAY - SPRING(MemoController_유효성 검증)

--- 04_DATA_VALIDATION/src/main/java/com/example/demo/Controller/MemoController.java
+++ 04_DATA_VALIDATION/src/main/java/com/example/demo/Controller/MemoController.java
@@ -2,8 +2,12 @@ package com.example.demo.Controller;
 
 
 import com.example.demo.Dtos.MemoDTO;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,20 +21,32 @@ public class MemoController {
     public void memoAdd() {
         log.info("GET /memo/add...");
     }
+
     @PostMapping("/add")
-    public void memoAddPost(MemoDTO memoDTO) {
+    public void memoAddPost(@Valid MemoDTO memoDTO, BindingResult result, Model model) {
         //1. 파라미터 받기
-        log.info("POST /memo/add..."+ memoDTO);
+        log.info("POST /memo/add..." + memoDTO);
+//        log.info("BindingResult : " + result);
+
         //2. 유효성 검증
+        if (result.hasErrors()) {
+            for (FieldError error : result.getFieldErrors()) {
+                log.info("Error Field : " + error.getField() + " Error Meesage : " + error.getDefaultMessage());
+                model.addAttribute(error.getField(), error.getDefaultMessage());
+            }
+        }
 
-        //3. 서비스 실행
+//            log.info("ERROR FIELD : " + result.getFieldError("id").getDefaultMessage());
 
-        //4. 뷰로 이동(+값)
+
+            //3. 서비스 실행
+
+            //4. 뷰로 이동(+값)
+
 
 
 
     }
-
 }
 
 
