d614acc20b68799ded0ac586300b536997fd8f90
pyj-0313 <pyjun0313@gmail.com>
2026-05-29 14:21:02 +0900

    03 DAY - SPRING(ExceptionTestController_예외처리)

--- 05_EXCEPTION/src/main/java/com/example/demo/Controller/ExceptionTestController.java
+++ 05_EXCEPTION/src/main/java/com/example/demo/Controller/ExceptionTestController.java
@@ -1,4 +1,28 @@
 package com.example.demo.Controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.FileNotFoundException;
+
+@Controller
+@Slf4j
+@RequestMapping("/except")
 public class ExceptionTestController {
+//    http://localhost:8090/except/ex1
+
+    @ExceptionHandler(FileNotFoundException.class)
+    public String exceptionHandler_1(Exception e){
+        log.error("ex1 : " + e);
+        return "except/error1";
+    }
+
+    @GetMapping("/ex1")
+    public void ex1() throws FileNotFoundException {
+        log.info("GET /except/ex1...");
+        throw new FileNotFoundException("파일을 찾을 수 없습니다..");
+    }
 }
