261f6d2bcd54a963ccab15db915ddaa26146e43c
pyj-0313 <pyjun0313@gmail.com>
2026-05-29 17:03:37 +0900

    03 DAY - SPRING(ThymeleafTestController_추가)

--- 06_THYMELEAF/src/main/java/com/example/demo/Controller/ThymeleafTestController.java
+++ 06_THYMELEAF/src/main/java/com/example/demo/Controller/ThymeleafTestController.java
@@ -1,4 +1,39 @@
 package com.example.demo.Controller;
 
+
+import com.example.demo.Dtos.PersonDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@Slf4j
+@RequestMapping("/th")
 public class ThymeleafTestController {
+    @GetMapping("/index/{isAuth}")
+    public void index(@PathVariable Boolean isAuth, Model model){
+        log.info("GET /th/index..");
+
+        //기본
+        model.addAttribute("name","홍길동");
+        //DTO
+        model.addAttribute("dto",new PersonDTO("남길동",55,"대구"));
+        //LIST<DTO>
+        List<PersonDTO> list = new ArrayList<>();
+        for(int i=0;i<10;i++){
+            list.add(new PersonDTO("TEST"+i,i,""));
+        }
+        model.addAttribute("list",list);
+        //분기처리
+        log.info("is auth ? " + isAuth);
+        model.addAttribute("isAuth",isAuth);
+
+
+    }
 }
