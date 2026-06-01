3e2fc9747685dd5906b7297398dae5619ed2f002
pyj-0313 <pyjun0313@gmail.com>
2026-06-01 12:11:53 +0900

    04 DAY - SPRING(MemoController.java)

---
+++ 07_DATASOURCE/src/main/java/com/example/demo/Controller/MemoController.java
@@ -0,0 +1,71 @@
+package com.example.demo.Controller;
+
+
+import com.example.demo.Domain.common.Daos.MemoDAO;
+import com.example.demo.Domain.common.Dtos.MemoDTO;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import java.beans.PropertyEditorSupport;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@Controller
+@Slf4j
+@RequestMapping("/memo")
+public class MemoController {
+
+    @Autowired
+    private MemoDAO memoDAO;
+
+    @ExceptionHandler
+    public String SQLExceptionHandler(Exception e, Model model){
+        log.error("MEMO SQLEXCEPTION.." + e);
+        model.addAttribute("ex",e.getMessage());
+        return "memo/error";
+    }
+
+    @GetMapping("/add")
+    public void memoAdd() {
+        log.info("GET /memo/add...");
+    }
+
+    @PostMapping("/add")
+    public String memoAddPost(@Valid MemoDTO memoDTO, BindingResult bindingResult, Model model) throws SQLException {
+        //1. 파라미터 받기
+        log.info("POST /memo/add..." + memoDTO);
+//        log.info("BindingResult : " + result);
+
+        //2. 유효성 검증
+        if (bindingResult.hasErrors()) {
+            for (FieldError error : bindingResult.getFieldErrors()) {
+                log.info("Error Field : " + error.getField() + " Error Message : " + error.getDefaultMessage());
+                model.addAttribute(error.getField(), error.getDefaultMessage());
+            }
+            return "memo/add";
+        }
+//            log.info("ERROR FIELD : " + result.getFieldError("id").getDefaultMessage());
+
+        //3. 서비스 실행
+        int result = memoDAO.insert(memoDTO);
+
+        //4. 뷰로 이동(+값)
+        return "memo/list";
+    }
+
+    @GetMapping("/list")
+    public void list_get(){
+        log.info("GET /memo/list...");
+
+    }
+}
+
+
