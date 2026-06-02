2bbaf6b0f69b7c708cb77cce3dc7ce72dcb04f37
pyj-0313 <pyjun0313@gmail.com>
2026-06-02 10:16:36 +0900

    05 DAY - SPRING

--- 08_SQLMAPPER_MYBATIS/src/test/java/com/example/demo/Config/MybatisConfigTest.java
+++ 08_SQLMAPPER_MYBATIS/src/test/java/com/example/demo/Config/MybatisConfigTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.Config;
 
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,17 +10,30 @@ import java.sql.Connection;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
 @SpringBootTest
 class MybatisConfigTest {
 
     @Autowired
     private SqlSessionFactory sqlSessionFactory;
 
+    @Autowired
+    private SqlSession sqlSession;
+
+    @Autowired
+    private MybatisConfig mybatisConfig;
     @Test
     public void t1(){
         assertNotNull(sqlSessionFactory);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        Connection conn = sqlSession.getConnection();
+        System.out.println(conn);
 
     }
 
+    @Test
+    public void t2(){
+        assertNotNull(sqlSession);
+        Connection conn = sqlSession.getConnection();
+        System.out.println(conn);
+    }
 }
