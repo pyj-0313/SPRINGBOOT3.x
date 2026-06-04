3d367eab1cdf6a6a6457708d4c8b966458582075
pyj-0313 <pyjun0313@gmail.com>
2026-06-04 10:19:59 +0900

    06 DAY - SPRING

--- 09_ORM_JPA/src/test/java/com/example/demo/Domain/Common/Entity/LendTest.java
+++ 09_ORM_JPA/src/test/java/com/example/demo/Domain/Common/Entity/LendTest.java
@@ -1,7 +1,45 @@
 package com.example.demo.Domain.Common.Entity;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.example.demo.Domain.Common.Repository.BookRepository;
+import com.example.demo.Domain.Common.Repository.LendRepository;
+import com.example.demo.Domain.Common.Repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
+
+@SpringBootTest
 class LendTest {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private BookRepository bookRepository;
+    @Autowired
+    private LendRepository lendRepository;
 
+
+    @Test
+    public void t1(){
+        //INSERT
+        User user = userRepository.findById("user1").get();
+        Book book = bookRepository.findById(1111L).get();
+        Lend lend = Lend.builder()
+                .user(user)
+                .book(book)
+                .build();
+        lendRepository.save(lend);
+    }
+    @Test
+    public void t2(){
+        //유저이름으로 대여 내역 조회
+//        List<Lend> list = lendRepository.findAllendsByUser("user1");
+//        list.forEach(System.out::println);
+
+        //도서이름으로 대여 내역 조회
+        List<Lend> list = lendRepository.findAllendsByBook("JAVA의정석");
+        list.forEach(System.out::println);
+
+    }
 }
