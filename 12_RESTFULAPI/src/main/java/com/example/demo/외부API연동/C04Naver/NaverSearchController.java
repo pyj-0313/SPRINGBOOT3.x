5109b498fc39142cb85cd808b6bc7b26b7dd20ef
pyj-0313 <pyjun0313@gmail.com>
2026-06-12 17:36:50 +0900

    12 DAY - SPRING(네이버검색)

--- 12_RESTFULAPI/src/main/java/com/example/demo/외부API연동/C04Naver/NaverSearchController.java
+++ 12_RESTFULAPI/src/main/java/com/example/demo/외부API연동/C04Naver/NaverSearchController.java
@@ -2,18 +2,55 @@ package com.example.demo.외부API연동.C04Naver;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 @Slf4j
 @RequestMapping("/Naver/search")
 public class NaverSearchController {
+
+    @Value("${naver.clientId}")
+    private String Client_Id;
+    @Value("${naver.clientSecret}")
+    private String CLIENT_SECRET;
+
     @GetMapping("/book/{keyword}")
-    public void search(@PathVariable String keyword)
+    @ResponseBody
+    public String search(@PathVariable String keyword)
     {
         log.info("GET /Naver.search...{}",keyword);
+
+        //요청파라미터 정리
+        String url = "https://openapi.naver.com/v1/search/book.json?query="+keyword;
+
+        //요청 헤더
+        HttpHeaders header = new HttpHeaders();
+        header.add("X-Naver-Client-Id", Client_Id);
+        header.add("X-Naver-Client-Secret", CLIENT_SECRET);
+
+//        MultiValueMap<String,String> params = new LinkedMultiValueMap<>();
+//        params.add("query",keyword);
+
+        //요청 엔터티(헤더 + 바디(params))
+//        HttpEntity< MultiValueMap<String,String> > entity = new HttpEntity<>(params,header);
+        HttpEntity entity = new HttpEntity<>(header);
+
+        //응답 = 요청
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET,entity,String.class);
+
+        System.out.println(response.getBody());
+
+        return response.getBody();
     }
 }
