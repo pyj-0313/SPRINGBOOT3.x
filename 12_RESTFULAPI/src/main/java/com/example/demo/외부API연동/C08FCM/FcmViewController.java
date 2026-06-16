419297fa8e606c726173d3f97bfeb5427d203495
pyj-0313 <pyjun0313@gmail.com>
2026-06-16 12:44:57 +0900

    14 DAY - SPRING(api추가)

--- 12_RESTFULAPI/src/main/java/com/example/demo/외부API연동/C08FCM/FcmViewController.java
+++ 12_RESTFULAPI/src/main/java/com/example/demo/외부API연동/C08FCM/FcmViewController.java
@@ -1,7 +1,9 @@
 package com.example.demo.외부API연동.C08FCM;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /*
@@ -11,6 +13,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @Slf4j
 public class FcmViewController {
+
+    @Value("${fire_apikey}")
+    private String apiKey;
+    @Value("${fire_domain}")
+    private String authDomain;
+    @Value("${fire_projectid}")
+    private String projectId;
+    @Value("${fire_bucket}")
+    private String storageBucket;
+    @Value("${fire_mesid}")
+    private String messagingSenderId;
+    @Value("${fire_appid}")
+    private String appId;
+    @Value("${fire_vapidkey}")
+    private String VAPID_KEY;
+
+
+
 
     // =========================================================================
     // [화면1] 발송 페이지 (제목/내용 입력 → /api/fcm/send 호출)
@@ -27,7 +47,15 @@ public class FcmViewController {
     //  - 접속: http://localhost:8080/fcm/receive
     // =========================================================================
     @GetMapping("/fcm/receive")
-    public String receive() {
+    public String receive(Model model) {
+        model.addAttribute("apiKey",apiKey);
+        model.addAttribute("authDomain",authDomain);
+        model.addAttribute("projectId",projectId);
+        model.addAttribute("storageBucket",storageBucket);
+        model.addAttribute("messagingSenderId",messagingSenderId);
+        model.addAttribute("appId",appId);
+        model.addAttribute("VAPID_KEY",VAPID_KEY);
+
         log.info("GET /fcm/receive....");
         return "fcm/receive"; // templates/fcm/receive.html
     }
