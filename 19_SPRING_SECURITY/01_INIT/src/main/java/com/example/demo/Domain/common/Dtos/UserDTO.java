3e78303108369714722916e8a01e275d53a54985
pyj-0313 <pyjun0313@gmail.com>
2026-06-18 16:08:06 +0900

    16 DAY - SPRING(UserDTO.java)

---
+++ 19_SPRING_SECURITY/01_INIT/src/main/java/com/example/demo/Domain/common/Dtos/UserDTO.java
@@ -0,0 +1,15 @@
+package com.example.demo.Domain.common.Dtos;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private String username;
+    private String password;
+    private String role;
+}
