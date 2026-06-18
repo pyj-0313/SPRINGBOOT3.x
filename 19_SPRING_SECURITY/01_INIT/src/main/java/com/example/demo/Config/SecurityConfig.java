f0783d588b02984d7833994c88a1f54482c84312
pyj-0313 <pyjun0313@gmail.com>
2026-06-18 15:09:23 +0900

    16 DAY - SPRING(권한처리)

--- 19_SPRING_SECURITY/01_INIT/src/main/java/com/example/demo/Config/SecurityConfig.java
+++ 19_SPRING_SECURITY/01_INIT/src/main/java/com/example/demo/Config/SecurityConfig.java
@@ -1,12 +1,34 @@
 package com.example.demo.Config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception
+    {
+        //CSRF 비활성화(비활성화하지 않으면 logout 요청은 기본적으로 POST방식을 따른다)
+        http.csrf((config)->{config.disable();});
 
+        //권한처리
+        http.authorizeHttpRequests((auth)->{
+            auth.requestMatchers("/","/join","/login").permitAll();
+        });
+
+
+        //로그인
+
+
+        //로그아웃
+
+
+        return http.build();
+    }
 }
